62da3de1478aaee5ffd81f3cd91d3fe29ebe0f23
Andres Martinez <amartinez@avanxo.com>
2018-05-14 03:41:51 -0500

    Badge/Repository added

---
+++ src/main/java/com/andrescode/trailheadbadgesapi/domain/Badge.java
@@ -0,0 +1,45 @@
+package com.andrescode.trailheadbadgesapi.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "badges")
+public class Badge {
+    @Id private String id;
+    private String type;
+    private String name;
+    private String href;
+    private Object hours;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public Object getHours() {
+        return hours;
+    }
+
+    public void setHours(float hours) {
+        this.hours = hours;
+    }
+}
---
+++ src/main/java/com/andrescode/trailheadbadgesapi/repository/BadgeRespository.java
@@ -0,0 +1,13 @@
+package com.andrescode.trailheadbadgesapi.repository;
+
+import com.andrescode.trailheadbadgesapi.domain.Badge;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "chrome-extension://gdlfghnokhjbfkdacdpnnckldihfcjdi")
+@RepositoryRestResource(collectionResourceRel = "badges", path = "badges")
+public interface BadgeRespository extends MongoRepository<Badge, String> {
+    Badge findByHref(@Param("href") String href);
+}
