c379ea37ae075aa26db92c2dd9f884a4f2d7d692
Andres Martinez <amartinez@avanxo.com>
2018-05-14 16:45:20 -0500

    Mongo embbeded added for testing

---
+++ src/test/java/com/andresscode/trailheadbadgesapi/repository/BadgeRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.andresscode.trailheadbadgesapi.repository;
+
+import com.andrescode.trailheadbadgesapi.Main;
+import com.andrescode.trailheadbadgesapi.domain.Badge;
+import com.andrescode.trailheadbadgesapi.repository.BadgeRespository;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
+public class BadgeRepositoryTest {
+    @Autowired
+    private BadgeRespository badgeRespository;
+
+    private Badge module;
+
+    @Test
+    public void findByHrefTest() {
+        Badge found = badgeRespository.findByHref(module.getHref());
+        Assert.assertEquals(module.getHref(), found.getHref());
+    }
+
+    @Before
+    public void setupBefore() {
+        module = new Badge();
+        module.setType("Module");
+        module.setName("Apex Module");
+        module.setHref("https://trailhead.salesforce.com/modules/apex_module");
+        module.setHours(1.83f);
+        badgeRespository.save(module);
+    }
+
+    @After
+    public void setupAfter() {
+        badgeRespository.delete(module);
+    }
+}
